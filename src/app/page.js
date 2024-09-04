27a80fda9b7bed8bb7d130e999611260a253cc49
DaviLMs <davilamarcateixeira@gmail.com>
2024-09-04 15:28:54 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,75 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'; // para funcionar e preciso ter isso para aparecer para o client
+import { useState } from "react";
 
 export default function Home() {
+  const [username, setUsername] = useState(""); // guarda um valor que possa ser alterado
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  async function login() { // assincrono
+    setLoading(true); // carregando
+    setError(null); // erro
+    setSuccessMessage(""); // mensagem de sucesso
+
+    const body = { //conteudo dos inputs
+      username, // nome
+      password, // senha
+      expiresInMins: 30, // tempo
+    };
+
+    try { // tentar
+      const response = await fetch('https://dummyjson.com/user/login', { // url
+        method: 'POST', // metodo
+        headers: { // cabecalho que faz a requisição de login
+          'Content-Type': 'application/json', //aqui passa o formato que e content-type
+        },
+        body: JSON.stringify(body), // aqui passa o conteudo que e o username e password
+      });
+
+      if (!response.ok) { // se o status for diferente de 200
+        throw new Error(`Erro no servidor: ${response.status}`); // mostre a mensagem de erro
+      }
+
+      const contentType = response.headers.get('Content-Type'); // pega o conteudo de resposta 
+
+      if (contentType && contentType.includes('application/json')) { // se o conteudo for json
+        const result = await response.json(); // pega o json
+        console.log(result); // mostra o json
+        setSuccessMessage(`Login realizado com sucesso: ${response.status}`); // mostra mensagem de sucesso
+
+      } else { // se o conteudo nao for json
+        const text = await response.text(); // pega o texto
+        throw new Error(`Formato de resposta inesperado: ${text}`); // mostra o erro
+      }
+    } catch (err) { // se der erro
+      setError(err.message); // mostra o erro
+    }
+
+    setLoading(false); // carregando
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div style={{ gap: '5px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1 }} >
+      <h1 style={{ width: '100%', textAlign: 'center' }}>Login</h1>
+      <input style={{ marginBottom: '20px' }}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+      />
+      <button onClick={login} disabled={loading}>
+        {loading ? <span>Loading...</span> : <h1>Login</h1>}
+      </button>
+      {successMessage && <p >{successMessage}</p>}
+      {error && <p >{error}</p>}
+    </div>
   );
 }
